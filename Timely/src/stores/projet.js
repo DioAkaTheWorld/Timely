056863e99855c77fe968bccf5639ab20c23002b7
Baptiste <delabordebaptiste8@gmail.com>
2026-02-03 11:55:09 +0100

    store projet

---
+++ Timely/src/stores/projet.js
@@ -0,0 +1,49 @@
+import { defineStore } from 'pinia'
+import api from '../api/index.js'
+import { useAuthStore } from './auth.js'
+
+export const useProjetStore = defineStore('projet', {
+    state(){
+        return {
+            projets: [],
+            projetSelect: null,
+        }
+    },
+
+    actions: {
+        async loadProject(){
+            const auth = useAuthStore();
+            try {
+                const response = await api.get('/api/projects', {
+                    headers: {
+                        Authorization: `key=${auth.apikey}`
+                    }
+                })
+
+                this.projets = response.data;
+            } catch (error) {
+                console.error('vous ne semblez pas avoir de projet pour le moment')
+            }
+        },
+
+        async ajouterProjet(name, description) {
+            const auth = useAuthStore();
+            try {
+                const response = await api.post('/api/projects', {
+                    name,
+                    description
+                },{
+                    headers: {
+                        Authorization: `key=${auth.apikey}`
+                    }
+                })
+
+                this.projets.push({name, description});
+            }catch (error) {
+                console.error("error");
+            }
+        }   
+    }
+
+
+})
