a59ec89a1ce10a21543ee6b64fa79b8715b29b06
Baptiste <delabordebaptiste8@gmail.com>
2026-02-12 10:25:53 +0100

    revu du router pour etre redirger sur la page login si pas de user connecter

--- Timely/src/router/index.js
+++ Timely/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import SettingsView from '../views/SettingsView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,5 +29,16 @@ const router = createRouter({
 
   ],
 })
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore()
+
+  if (to.name !== 'login' && !auth.apikey) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
+})
+
 
 export default router
