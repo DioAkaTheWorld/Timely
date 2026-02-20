eb1872574a5c840a2a796a1869407f3ab9e132b2
DioAkaTheWorld <burak.ozen7@etu.univ-lorraine.fr>
2026-02-20 11:03:14 +0100

    Création StatsView

---
+++ Timely/src/views/StatsView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted, nextTick, computed } from 'vue';
+import { useStatsStore } from '@/stores/stats';
+import { useProjetStore } from '@/stores/projet';
+import { useActiviteStore } from '@/stores/activite';
+import Chart from 'chart.js/auto';
+
+// Stores
+const statsStore = useStatsStore();
+const projetStore = useProjetStore();
+const activiteStore = useActiviteStore();
+
+</script>
+
+
+<template>
+  <h1>Vue Stats</h1>
+</template>
